f94766ae226b108c942802f9cbb98915cfedfb15
sixLUKEY <marcoslukerojo@gmail.com>
2023-05-24 20:57:05 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// let celsius = document.getElementById('celsius');
+// let fahrenheit = document.getElementById('fahrenheit');
+
+let celsius= 50;
+let fahrenheit = 122;
+
+let fahrenheitCalc = (celsius*9/5) +32;
+console.log(fahrenheitCalc);
+
+let celsiusCalc = (fahrenheit -32)*5/9;
+console.log(celsiusCalc);
